0fc80e14c5f3288b3ac6b07d3ec04b975bb3a5ec
MdTasib <ohidulalam442@gmail.com>
2022-12-04 22:44:36 +0600

    create first query

--- src/components/videos/Videos.js
+++ src/components/videos/Videos.js
@@ -1,15 +1,33 @@
 import Video from "./Video";
+import { useGetVideosQuery } from "../../features/api/apiSlice";
+import VideoLoading from "../ui/loaders/VideoLoader";
+import Error from "../ui/Error";
 
 export default function Videos() {
-    return (
-        <>
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-            <Video />
-        </>
-    );
+	const { data: videos, isLoading, isError } = useGetVideosQuery();
+
+	let content = null;
+	if (isLoading) {
+		content = (
+			<>
+				<VideoLoading />
+				<VideoLoading />
+				<VideoLoading />
+				<VideoLoading />
+			</>
+		);
+	}
+
+	if (!isLoading && isError) {
+		content = <Error message='There was an error' />;
+	}
+
+	if (!isError && !isLoading && videos?.length === 0) {
+		content = <Error message='No videos found!' />;
+	}
+	if (!isError && !isLoading && videos?.length > 0) {
+		content = videos?.map(video => <Video key={video.id} video={video} />);
+	}
+
+	return <>{content}</>;
 }
